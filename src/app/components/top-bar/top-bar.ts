e2bf6cc1936d8c9be05cb981f03d2225838d9c92
viniladapa <vinil.adapa@iprotechs.com>
2026-03-31 22:17:10 +0530

    feat: enhance top bar with search and notification dropdowns, add dark mode toggle, and improve layout styling

--- src/app/components/top-bar/top-bar.ts
+++ src/app/components/top-bar/top-bar.ts
@@ -1,18 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-top-bar',
-  imports: [RouterLink],
+  imports: [RouterModule, CommonModule],
   templateUrl: './top-bar.html',
   styleUrl: './top-bar.scss',
 })
 export class TopBar {
 
+  showProfile: boolean = false;
+  showNotifications: boolean = false;
+  showSettings: boolean = false;
+  darkMode: boolean = false;
+
+  toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+    this.showProfile = false;
+  }
+
+  toggleProfile() {
+    this.showProfile = !this.showProfile;
+    this.showNotifications = false;
+  }
+
   toggleSidemenu() {
-    document.querySelector('#side-menu')?.classList.toggle('sidemenu--hide');
-    document.querySelector('#top-bar')?.classList.toggle('top-bar--full-width');
-    document.querySelector('.main-content')?.classList.toggle('full-screen');
-    document.querySelector('.close-icon')?.classList.toggle('show');
+    document.querySelector('.left-panel')?.classList.toggle('sidebar-collapsed');
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark-mode', this.darkMode);
+  }
+
+  logout() {
+    console.log('logout');
   }
 }
