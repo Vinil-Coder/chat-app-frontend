162ae5014c2183b7ae35ef256a07f4295b98c817
viniladapa <vinil.adapa@iprotechs.com>
2026-04-02 12:33:35 +0530

    feat: implement reset password page with form validation and styling

--- src/app/pages/resetpassword/resetpassword.ts
+++ src/app/pages/resetpassword/resetpassword.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-resetpassword',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './resetpassword.html',
   styleUrl: './resetpassword.scss',
 })
